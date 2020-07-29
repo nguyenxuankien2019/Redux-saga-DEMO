33f361caa7f071d69f91ed91c5b30b79b5194f37
nguyenxuankien2019 <nguyenxuankien1997@gmail.com>
2020-07-29 11:13:08 +0700

    200 - inputprops

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements'
 import Spacer from '../Components/Spacer';
-class SigninScreen extends Component {
-    constructor(props) {
-        super(props);
-    }
+const SigninScreen = ({ navigation }) => {
+    const [email,setEmail] =useState('');
+    const [password,setPassword] =useState('');
 
-    render() {
-        return (
-            <>
-                <View style={styles.container}>
-                    <View style={styles.spacer} />
-                    <Text h3>Sign up for Tracker</Text>
-                    <View style={styles.spacer} />
-                    <Input label='Email' />
-                    <View style={styles.spacer} />
-                    <Input label='Password' />
-                    <View style={styles.spacer} />
-                    <Button containerStyle={styles.loginBtn} title='Sign up' />
-                </View>
-            </>
-        );
-    }
+    return (
+        <>
+            <View style={styles.container}>
+                <View style={styles.spacer} />
+                <Text h3>Sign up for Tracker</Text>
+                <View style={styles.spacer} />
+
+                <Input 
+                label='Email'
+                value={email}
+                onChangeText={setEmail}
+                autoCorrect={false}
+                autoCapitalize='none' />
+                <View style={styles.spacer} />
+
+                <Input 
+                label='Password' 
+                value={password}
+                onChangeText={setPassword}
+                autoCorrect={false}
+                autoCapitalize='none' 
+                secureTextEntry />
+                <View style={styles.spacer} />
+                <Button containerStyle={styles.loginBtn} title='Sign up' />
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -32,8 +42,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
-    loginBtn:{
-        width:'80%'
+    loginBtn: {
+        width: '80%'
     },
     spacer: { margin: 10 }
 
