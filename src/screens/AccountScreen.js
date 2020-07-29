f61ec7e938f0c1449483cfd26643f5068abec2ae
nguyenxuankien2019 <nguyenxuankien1997@gmail.com>
2020-07-29 10:20:01 +0700

    196 add react-native-elements

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Image, Text, FlatList, SafeAreaView,TouchableOpacity } from 'react-native';
-
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 class AccountScreen extends Component {
     constructor(props) {
         super(props);
@@ -13,6 +14,56 @@ class AccountScreen extends Component {
               <TouchableOpacity onPress={()=>this.props.navigation.navigate('TrackList')}>
                   <Text>Go to TrackListFlow</Text>
               </TouchableOpacity>
+              <Button
+  title="Solid Button"
+/>
+
+<Button
+  title="Outline button"
+  type="outline"
+/>
+
+<Button
+  title="Clear button"
+  type="clear"
+/>
+
+<Button
+  icon={
+    <Icon
+      name="arrow-right"
+      size={15}
+      color="white"
+    />
+  }
+  title="Button with icon component"
+/>
+
+<Button
+  icon={{
+    name: "arrow-right",
+    size: 15,
+    color: "white"
+  }}
+  title="Button with icon object"
+/>
+
+<Button
+  icon={
+    <Icon
+      name="arrow-right"
+      size={15}
+      color="white"
+    />
+  }
+  iconRight
+  title="Button with right icon"
+/>
+
+<Button
+  title="Loading button"
+  loading
+/>
           </SafeAreaView>
         );
     }
