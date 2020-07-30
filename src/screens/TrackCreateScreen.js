e76b08797b156161a5f83788d76cd0ac74ef10c0
nguyenxuankien2019 <nguyenxuankien1997@gmail.com>
2020-07-30 13:54:21 +0700

    226 -drawing points

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -25,8 +25,6 @@ class SigninScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
 
