30468707f23cd7cd05202e83df65873ab3fa488c
nguyenxuankien2019 <nguyenxuankien1997@gmail.com>
2020-07-30 12:19:42 +0700

    update

--- src/screens/Components/Map.js
+++ src/screens/Components/Map.js
@@ -1,17 +1,25 @@
 import React, {  } from 'react';
 import { View, StyleSheet,  TouchableOpacity, Text } from 'react-native';
-// import MapView from 'react-native-maps';
+import MapView from 'react-native-maps';
 
 const Map = () => {
 
     return (         
-        <View style={styles.map}/>
+        <MapView
+        initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        style={styles.map}/>
     );
 }
 
 const styles = StyleSheet.create({
     map:{
-        height:300
+        height:300,
+        backgroundColor:'red'
     }
 })
 
